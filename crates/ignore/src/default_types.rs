dabe083ae7a9627340aade67423fa04573f5e1bd
Bryce Schultz <bryceschultz@live.com>
2024-09-20 21:34:20 -0600

    added .csf support

--- crates/ignore/src/default_types.rs
+++ crates/ignore/src/default_types.rs
@@ -53,6 +53,7 @@ pub(crate) const DEFAULT_TYPES: &[(&[&str], &[&str])] = &[
     (&["creole"], &["*.creole"]),
     (&["crystal"], &["Projectfile", "*.cr", "*.ecr", "shard.yml"]),
     (&["cs"], &["*.cs"]),
+    (&["csf"], &["*.csf"]),
     (&["csharp"], &["*.cs"]),
     (&["cshtml"], &["*.cshtml"]),
     (&["csproj"], &["*.csproj"]),
